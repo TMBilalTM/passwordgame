2594ea0e394c79036d55954096d221da18df8606
Bilal <112407129+TMBilalTM@users.noreply.github.com>
2024-08-06 18:24:39 +0300

    Update RuleMorse.jsx

--- rules/RuleMorse/RuleMorse.jsx
+++ rules/RuleMorse/RuleMorse.jsx
@@ -1,29 +1,20 @@
 import Rule from "../Rule";
 
-const morse = {
-    a: ".-", b: "-...", c: "-.-.", d: "-..", e: ".", f: "..-.", g: "--.", h: "....",
-    i: "..", j: ".---", k: "-.-", l: ".-..", m: "--", n: "-.", o: "---", p: ".--.", 
-    q: "--.-", r: ".-.", s: "...", t: "-", u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--", z: "--.." 
-}
-
-export default class RuleMorse extends Rule{
-    constructor(){
-        super("Şifrenizde ilk 3 İngilizce alfabenin Mors kodu bulunmalıdır. (. ve - kullanın)");
+export default class RuleMorse extends Rule {
+    constructor() {
+        super("Şifrenizde 12. ve 14. elementlerin birleşimi olmalıdır ifadesi bulunmalıdır.");
     }
 
-    check(txt){
-        let letters = txt.match(/[A-Za-z]/g)?.slice(0, 3);
-        
-        if (letters?.length === 3) {
-            let code = `${morse[letters[0].toLowerCase()]} ${morse[letters[1].toLowerCase()]} ${morse[letters[2].toLowerCase()]}`;
-            let exp = code.replace(/\./g, "\\.").replace(/-/g, "\\-");
+    check(txt) {
+        // Şifreyi küçük harfe çevir
+        const lowerTxt = txt.toLowerCase();
 
-            console.log("morse:", exp);
-            let r = new RegExp(exp);
-            
-
-            return r.test(txt);
-        }
-        return false;
+        // İlgili ifadeleri kontrol et
+        return (
+            lowerTxt.includes("mgsi") ||
+            lowerTxt.includes("magnezyumsilisyum") ||
+            txt.includes("MgSi") ||
+            txt.includes("MAGNEZYUMSILISYUM")
+        );
     }
 }
